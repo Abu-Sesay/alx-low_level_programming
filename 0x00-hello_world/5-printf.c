744387a09dba3e80ecd5cbfee6c07406bf43bb4d
Abu-Sesay <sesayabu64@yahoo.com>
2022-06-03 22:32:24 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  * main Entry point
  * Retun: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
