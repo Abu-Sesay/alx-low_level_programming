f82dcf80c9d5f6d38e912b8817c5b868fcffd6d0
Abu-Sesay <sesayabu64@yahoo.com>
2022-06-10 09:16:41 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * _isupper - Enter point
+ * @c: character to be checked
+ *
+ * Return: Always 0.
+ */
+int main(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
