74c270a8d17f48c8b27942a01637f3da4bb14544
Abu-Sesay <sesayabu64@yahoo.com>
2022-06-10 07:13:34 +0000

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _fizzbuzz - checks if character is fizz or buzz or bothe
+ *
+ * Return: Always 0 (Success)
+ */
+int _fizzbuzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("Fizz Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
