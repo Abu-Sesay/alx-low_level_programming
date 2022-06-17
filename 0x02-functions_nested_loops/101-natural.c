825eb3c538135b9f4affaec905ed7e6612ad0475
Abu-Sesay <sesayabu64@yahoo.com>
2022-06-17 11:12:49 +0000

    Task 12

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - enter point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+			b = b + a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
