a00b9b7a17b91fce918aefefaf8ee40742130b48
Abu-Sesay <sesayabu64@yahoo.com>
2022-06-17 14:37:58 +0000

    Task 5

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 int _isupper(int c);
 int _fizz_buzz(void);
 void print_numbers(void);
