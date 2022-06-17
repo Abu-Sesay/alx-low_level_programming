e617732f0d250df0598815035fa40e800a23b6d7
Abu-Sesay <sesayabu64@yahoo.com>
2022-06-17 06:24:40 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Enter point
+ * @a: Character to be added
+ * @b: The other character to be added
+ *
+ *Return: Result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
