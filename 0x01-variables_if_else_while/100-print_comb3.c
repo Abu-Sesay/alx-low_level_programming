9b1a1ae106abdde4f8a59301423924df71a5d7ac
Abu-Sesay <sesayabu64@yahoo.com>
2022-09-09 22:09:28 +0000

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -9,16 +10,19 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 48; i <= 56; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		for (j = 49; j <= 57; j++)
+		for (j = 48; j <= 57; j++)
 		{
+			if (j > i)
+			{
 			putchar(i);
 			putchar(j);
-			if (i != 56 || j != 57)
+			if (i != 56)
 			{
-				putchar(',');
-				putchar(' ');
+			putchar(44);
+			putchar(32);
+			}
 			}
 		}
 	}
