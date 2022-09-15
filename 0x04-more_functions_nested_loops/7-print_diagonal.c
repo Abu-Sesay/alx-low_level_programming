c2f21b98d3d7833296e2ab522eda7c7da951862c
Abu-Sesay <sesayabu64@yahoo.com>
2022-09-15 10:39:56 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: character to compute
+ * Return: none
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				if (i == j)
+				{
+					_putchar(92);
+				}
+				else
+				{
+					_putchar(32);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
