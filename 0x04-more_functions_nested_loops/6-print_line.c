dcf883a0ab641a2ac07a92595654ddf7890659bd
Abu-Sesay <sesayabu64@yahoo.com>
2022-09-15 10:21:08 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Output the value
+ * @n: character to be checked
+ * Return: none
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
