bf0cf68d59a0b11f1e2686352a6f6714e4152353
Abu-Sesay <sesayabu64@yahoo.com>
2022-09-15 08:08:20 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Multiply the code
+ * @a: one of the char to be multiply
+ * @b: another char to be multiplied
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
