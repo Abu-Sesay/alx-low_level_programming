11fed39d921aac9cd89facd98ff3b4c147592f3c
Abu-Sesay <sesayabu64@yahoo.com>
2022-09-21 16:18:42 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - The function to convert to upper
+ * Return: chr
+ */
+char *string_toupper(char *s)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] >= 97 && s[j] <= 122)
+		{
+			s[j] = s[j] - 32;
+		}
+	}
+	return (s);
+}
