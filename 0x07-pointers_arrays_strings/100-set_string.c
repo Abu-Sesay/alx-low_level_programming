2a4ceb36ab6530debfaee066154b8f19906bdab0
Abu-Sesay <sesayabu64@yahoo.com>
2022-09-29 06:47:02 -0700

    Task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value pointer
+ * @s: Pointer to pointer
+ * @to: character
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
