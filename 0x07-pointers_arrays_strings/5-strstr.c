0b56d6c3f49f66815c21cfe2a8a51cfe3b10c4b7
Abu-Sesay <sesayabu64@yahoo.com>
2022-09-28 18:06:23 -0700

    hot fixed

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
  * *_strstr - locates a substring
  * @haystack: the string pointed to
  * @needle: substring
- * 
+ *
  * Return: pointer to beginning of substring
  */
 
@@ -14,6 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		char *i = haystack;
 		char *j = needle;
+
 		while (*haystack && *j && *haystack == *j)
 		{
 			haystack++;
